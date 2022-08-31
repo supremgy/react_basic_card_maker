acc1dc9c8a5c50f6489b95089da5149239e6ab39
supremgy <supremgy@icloud.com>
2022-08-31 17:34:45 +0900

    add card add form component

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -3,7 +3,7 @@ import Button from '../button/button';
 import ImageFileInput from '../image_file_input/image_file_input';
 import styles from './card_edit_form.module.css';
 
-const Card_edit_form = ({ card }) => {
+const CardEditForm = ({ card }) => {
   const { name, company, title, email, message, theme, filename, fileURL } =
     card;
   const onSubmit = () => {};
@@ -18,9 +18,9 @@ const Card_edit_form = ({ card }) => {
         value={company}
       />
       <select className={styles.select} name="theme" value={theme}>
-        <option value="light">Light</option>
-        <option value="dark">Dark</option>
-        <option value="colorful">Colorful</option>
+        <option value="light">light</option>
+        <option value="dark">dark</option>
+        <option value="colorful">colorful</option>
       </select>
       <input className={styles.input} type="text" name="title" value={title} />
       <input className={styles.input} type="text" name="email" value={email} />
@@ -34,4 +34,4 @@ const Card_edit_form = ({ card }) => {
   );
 };
 
-export default Card_edit_form;
+export default CardEditForm;
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -24,7 +24,7 @@ const Maker = ({ authService }) => {
       id: '2',
       name: 'Aya',
       company: 'Apple',
-      theme: 'light',
+      theme: 'colorful',
       title: 'Software Engineer',
       email: 'supremgy@icloud.com',
       message: 'Just DO!',
@@ -35,7 +35,7 @@ const Maker = ({ authService }) => {
       id: '3',
       name: 'Noah3',
       company: 'Apple',
-      theme: 'dark',
+      theme: 'light',
       title: 'Software Engineer',
       email: 'supremgy@icloud.com',
       message: 'Just DO!',
@@ -55,11 +55,16 @@ const Maker = ({ authService }) => {
       }
     });
   });
+
+  const addCard = (card) => {
+    const updated = [...cards, card];
+    setCards(updated);
+  };
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor cards={cards} />
+        <Editor cards={cards} addCard={addCard} />
         <Preview cards={cards} />
       </div>
       <Footer />
