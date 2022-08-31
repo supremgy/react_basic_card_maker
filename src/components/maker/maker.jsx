69ac9fe0b941543cb3a5e19a27635c89aca34281
supremgy <supremgy@icloud.com>
2022-08-31 16:22:53 +0900

    add card and edit from component

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Editor from '../editor/editor';
@@ -7,6 +8,41 @@ import Header from '../header/header';
 import Preview from '../preview/preview';
 import styles from './maker.module.css';
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: '1',
+      name: 'Noah',
+      company: 'Apple',
+      theme: 'dark',
+      title: 'Software Engineer',
+      email: 'supremgy@icloud.com',
+      message: 'Just DO!',
+      fileName: 'noah',
+      fileURL: null,
+    },
+    {
+      id: '2',
+      name: 'Aya',
+      company: 'Apple',
+      theme: 'light',
+      title: 'Software Engineer',
+      email: 'supremgy@icloud.com',
+      message: 'Just DO!',
+      fileName: 'noah',
+      fileURL: null,
+    },
+    {
+      id: '3',
+      name: 'Noah3',
+      company: 'Apple',
+      theme: 'dark',
+      title: 'Software Engineer',
+      email: 'supremgy@icloud.com',
+      message: 'Just DO!',
+      fileName: 'noah',
+      fileURL: null,
+    },
+  ]);
   const navigate = useNavigate();
   const onLogout = () => {
     authService.logout();
@@ -23,8 +59,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
